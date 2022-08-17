3d72fecdf217f9af3bdaf0a58b1e92e293de1ea1
sudhakarsn15 <sudhakarsn15@gmail.com>
2022-08-17 12:59:54 +0000

    Added handelTextChangeFnc and postMessageFnc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [inputText, setInputText] = useState("");
+  const handelTextChange = (e) => {
+    setInputText(e.target.value);
+    // console.log(e.target.value);
+  };
+
   const postMessageFnc = () => {
-    alert("here");
+    window.postMessage(inputText);
+    // alert("here : " + inputText);
   };
 
   return (
     <div className="App">
       <h1>Window.postMessage() Example</h1>
-      <input type="text"></input>
+      <input
+        type="text"
+        value={inputText}
+        // onChange={(e) => setInputText(e.target.value)}
+        onChange={(e) => handelTextChange(e)}
+      ></input>
       <button type="button" onClick={() => postMessageFnc()}>
         Post Message
       </button>
